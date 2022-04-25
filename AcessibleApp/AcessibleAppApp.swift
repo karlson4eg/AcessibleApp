6d6fd4d5a11df63c098cdc7445ceb71405a7775a
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-25 18:36:18 +0400

    first commit, added button description

---
+++ AcessibleApp/AcessibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcessibleAppApp.swift
+//  AcessibleApp
+//
+//  Created by Evi St on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AcessibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
